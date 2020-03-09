fe5bba248603e2304ab3e605484b095820ada11c
marvio-da-rosa-freire <61966476+marvio-da-rosa-freire@users.noreply.github.com>
2020-03-09 20:07:38 +0100

    Update signals.js

--- signals.js
+++ signals.js
@@ -1,6 +1,7 @@
+/***************************************************************************
 The MIT License (MIT)
 
-Copyright (c) 2020 Marvio da Rosa Freire
+Copyright (c) 2020 Marvio Da Rosa Freire
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -19,6 +20,7 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
+*****************************************************************************/
 
 
 //binding value; text value, class, css property , whatever...
